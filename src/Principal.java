04c979d0c88ac5f38411d72a651a7a7badd0deca
Kaiwawa <rafael_c_baumann@hotmail.com>
2022-11-17 16:25:33 -0300

    Resto

--- src/Principal.java
+++ src/Principal.java
@@ -1,11 +1,28 @@
-public class Principal {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Principal {
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        
+        double x;
+        double y;
+        
+        System.out.println("Digite o valor de X:");
+        x = Double.parseDouble(reader.readLine());
+        System.out.println("Digite o valor de Y:");
+        y = Double.parseDouble(reader.readLine());
+        
+        Resto r = new Resto();
+        System.out.println(r.resto(x, y));
+         
+        
         Quadrado q = new Quadrado();
         q.areaQuadrado(4.25);
         System.out.println();
         Velocidade v = new Velocidade();
         v.velocidadeMedia(200, 0, 60, 0);
-
     }
 }
---
+++ src/Resto.java
@@ -0,0 +1,7 @@
+public class Resto {
+
+    public double resto(double x, double y){
+        double resto = x % y;
+        return resto;
+    }
+}
