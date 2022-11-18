944bee08548370634802ada21609c328b84ffa47
Célio Júnior <celio_cjr@hotmail.com>
2022-11-18 17:31:44 -0300

    Descomentando as execuções na classe Principal. Instância das classes estão no final da função.

--- src/Principal.java
+++ src/Principal.java
@@ -5,60 +5,60 @@ import java.io.InputStreamReader;
 public class Principal {
     static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-//    static double x;
-//    static double y;
-//    static double z;
+    static double x;
+    static double y;
+    static double z;
 
-//    public static double perguntax() throws NumberFormatException, IOException{
-//        double x;
-//        System.out.println("Digite o valor de X:");
-//        x = Double.parseDouble(reader.readLine());
-//        return x;
-//    }
-//
-//    public static double perguntay() throws NumberFormatException, IOException{
-//        double y;
-//        System.out.println("Digite o valor de Y:");
-//        y = Double.parseDouble(reader.readLine());
-//        return y;
-//    }
-//
-//    public static double perguntaz() throws NumberFormatException, IOException{
-//        double z;
-//        System.out.println("Digite o valor de Z:");
-//        z = Double.parseDouble(reader.readLine());
-//        return z;
-//    }
+    public static double perguntax() throws NumberFormatException, IOException{
+        double x;
+        System.out.println("Digite o valor de X:");
+        x = Double.parseDouble(reader.readLine());
+        return x;
+    }
+
+    public static double perguntay() throws NumberFormatException, IOException{
+        double y;
+        System.out.println("Digite o valor de Y:");
+        y = Double.parseDouble(reader.readLine());
+        return y;
+    }
+
+    public static double perguntaz() throws NumberFormatException, IOException{
+        double z;
+        System.out.println("Digite o valor de Z:");
+        z = Double.parseDouble(reader.readLine());
+        return z;
+    }
     
     public static void main(String[] args) /*throws NumberFormatException, IOException*/ {
         
-//        x = perguntax();
-//        y = perguntay();
-//        z = perguntaz();
-//
-//        Bhaskara.bhaskara(x, y, z);
-//        String result[] = Bhaskara.bhaskara(x, y, z);
-//        System.out.println("X1 = " + result[0] + "\nX2 = " + result[1]);
-//
-//        x = Double.parseDouble(result[0]);
-//        y = Double.parseDouble(result[1]);
-//
-//        System.out.println("Somando X1 e X2 temos: " + BCalc.bcalc_soma(x, y));
-//        System.out.println("Dividindo X1 e X2 temos: " + BCalc.bcalc_divi(x, y));
-//
-//
-//        Quadrado q = new Quadrado();
-//        q.areaQuadrado(4.25);
-//        System.out.println();
-//        Velocidade v = new Velocidade();
-//        v.velocidadeMedia(200, 0, 60, 0);
+        x = perguntax();
+        y = perguntay();
+        z = perguntaz();
 
-//        CalculoMedia cm = new CalculoMedia();
-//        cm.calcularMedia();
+        Bhaskara.bhaskara(x, y, z);
+        String result[] = Bhaskara.bhaskara(x, y, z);
+        System.out.println("X1 = " + result[0] + "\nX2 = " + result[1]);
+
+        x = Double.parseDouble(result[0]);
+        y = Double.parseDouble(result[1]);
+
+        System.out.println("Somando X1 e X2 temos: " + BCalc.bcalc_soma(x, y));
+        System.out.println("Dividindo X1 e X2 temos: " + BCalc.bcalc_divi(x, y));
+
+
+        Quadrado q = new Quadrado();
+        q.areaQuadrado(4.25);
+        System.out.println();
+        Velocidade v = new Velocidade();
+        v.velocidadeMedia(200, 0, 60, 0);
+
+        // EXECUÇÃO NO TERMINAL
+        // A depender da versão do Java e do SDK, colocar vírgula ao "," de "."
+        CalculoMedia cm = new CalculoMedia();
+        cm.calcularMedia();
 
         CalculoGorjeta cg = new CalculoGorjeta();
         cg.calcularGorjeta();
-
-
     }
 }
